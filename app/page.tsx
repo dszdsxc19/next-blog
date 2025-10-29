55a593bf28911f710d4ba2a5900e103473fc81ef
dszdsxc19 <dszdsxc19@gmail.com>
2025-10-29 19:28:44 +0800

    Remove unused design documents and .idea/.gitignore
    - Delete `.idea/.gitignore` as it is no longer needed
    - Remove outdated design documents for `author-activity-visualizations`, `blog-toc-feature`, and `toc-scroll-visibility`

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { filterRegularPosts } from '@/lib/blog'
 import Main from './Main'
 
 export default async function Page() {
+  // @ts-ignore
   const sortedPosts = sortPosts(filterRegularPosts(allBlogs))
   const posts = allCoreContent(sortedPosts)
   return <Main posts={posts} />
