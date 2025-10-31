a028f4a786054b847390c8225db5181159a9872c
dszdsxc19 <dszdsxc19@gmail.com>
2025-10-31 22:46:18 +0800

    feat: eslint ignoreDuringBuilds

--- next.config.js
+++ next.config.js
@@ -86,12 +86,16 @@ module.exports = () => {
         },
       ]
     },
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
     webpack: (config, options) => {
       config.module.rules.push({
         test: /\.svg$/,
         use: ['@svgr/webpack'],
       })
-
       return config
     },
   })
